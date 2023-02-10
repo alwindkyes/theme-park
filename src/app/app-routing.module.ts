43e38b3bdb11c0fccc7b62fc617208cecdc0e15d
alwindhinagaran <alwindhinagaran@hakunamatata.in>
2023-02-10 15:40:39 +0530

    Creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'booking', loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule) }, { path: 'billing', loadChildren: () => import('./billing/billing.module').then(m => m.BillingModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
